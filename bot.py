f94cf809d4b10aa19fca38cf01759632b96b584a
Jameson Orvis <jameson.orvis@gmail.com>
2020-05-16 22:35:51 -0400

    add environmental variables

--- bot.py
+++ bot.py
@@ -5,7 +5,13 @@
 import tweepy
 import schedule
 import time
+from os import environ
 from make_tweet import make_tweet
+
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
 def job(): 
     tweet = make_tweet()
@@ -13,8 +19,7 @@ def job():
     auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
     api = tweepy.API(auth)
     api.update_status(tweet)
-    print(tweet)
-    
+
 schedule.every().minute.at(":15").do(job)
 
 while True:
